b518ed8f95f5e3c1bc1fbebc62e4e41308042859
Eren <d.erenzirekbilek@hotmail.com>
2024-04-22 22:52:02 +0300

    Remove logo, update App component with Tic Tac Toe game and style changes, and remove index.css
    * Delete logo.svg and its import in App.js
    * Implement Tic Tac Toe game in App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <section>
+      <h1>Tic Tac Toe</h1>
+      <hr/>
+     </section>
+     <div>
+        <div>
+                <input type="text" id= "b1" 
+                       className="cell" onclick="myfunc_3(); myfunc();" 
+                       /> 
+                <input type="text" id= "b2" 
+                       className="cell" onclick="myfunc_4(); myfunc();" 
+                       /> 
+                <input type="text" id= "b3" class="cell" 
+                       onclick="myfunc_5(); myfunc();" 
+                       /> 
+        </div>
+        <div className="row"> 
+                <input type="text" id= "b4" 
+                       className="cell" onclick="myfunc_6(); myfunc();" 
+                       /> 
+                <input type="text" id= "b5" 
+                       className="cell" onclick="myfunc_7(); myfunc();" 
+                       /> 
+                <input type="text" id= "b6" 
+                       className="cell" onclick="myfunc_8(); myfunc();" 
+                       /> 
+        </div> 
+        <div className="row"> 
+                <input type="text" id= "b7" 
+                       className="cell" onclick="myfunc_9(); myfunc();" 
+                       /> 
+                <input type="text" id= "b8" 
+                       className="cell" onclick="myfunc_10();myfunc();" 
+                       /> 
+                <input type="text" id= "b9" 
+                       className="cell" onclick="myfunc_11();myfunc();" 
+                       /> 
+        </div> 
+      </div>
+      <br/><br/><br/> 
+      <button id="but" onclick="myfunc_2()"> 
+            RESET 
+        </button> 
     </div>
   );
 }
